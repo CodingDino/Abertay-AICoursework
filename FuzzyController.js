b0986dad2f8c5073bfbf06612ef0c05f38f5ec77
Sarah Herzog <sarah.ann.herzog@gmail.com>
2012-12-24 15:40:30 +0000

    Ignores curviness for defuzzification, temporary solution.

--- FuzzyController.js
+++ FuzzyController.js
@@ -90,15 +90,6 @@ function FuzzyController() {
 		this.position.sets[0].memfunc.rbp = -250;
 		this.position.sets[0].memfunc.rc = 5;
 		
-		// OLD CODE
-		// this.memfunc_pos_farleft_lbp = -650;
-		// this.memfunc_pos_farleft_lpp = -650;
-		// this.memfunc_pos_farleft_lc = 5;
-		// this.memfunc_pos_farleft_rpp = -350;
-		// this.memfunc_pos_farleft_rbp = -250;
-		// this.memfunc_pos_farleft_rc = 5;
-		//memfunc_pos_farleft_name = "Far Left";
-		
 		// Left Member Function
 		this.position.sets[1].memfunc = new Object();
 		this.position.sets[1].memfunc.lbp = -350;
@@ -107,15 +98,6 @@ function FuzzyController() {
 		this.position.sets[1].memfunc.rpp = -150;
 		this.position.sets[1].memfunc.rbp = -50;
 		this.position.sets[1].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_pos_left_lbp = -350;
-		// this.memfunc_pos_left_lpp = -250;
-		// this.memfunc_pos_left_lc = 5;
-		// this.memfunc_pos_left_rpp = -150;
-		// this.memfunc_pos_left_rbp = -50;
-		// this.memfunc_pos_left_rc = 5;
-		// this.memfunc_pos_left_name = "Left";
 		
 		// Center Member Function
 		this.position.sets[2].memfunc = new Object();
@@ -126,15 +108,6 @@ function FuzzyController() {
 		this.position.sets[2].memfunc.rbp = 150;
 		this.position.sets[2].memfunc.rc = 5;
 		
-		// OLD CODE
-		// this.memfunc_pos_center_lbp = -150;
-		// this.memfunc_pos_center_lpp = -50;
-		// this.memfunc_pos_center_lc = 5;
-		// this.memfunc_pos_center_rpp = 50;
-		// this.memfunc_pos_center_rbp = 150;
-		// this.memfunc_pos_center_rc = 5;
-		// this.memfunc_pos_center_name = "Center";
-		
 		// Right Member Function
 		this.position.sets[3].memfunc = new Object();
 		this.position.sets[3].memfunc.lbp = 50;
@@ -143,15 +116,6 @@ function FuzzyController() {
 		this.position.sets[3].memfunc.rpp = 250;
 		this.position.sets[3].memfunc.rbp = 350;
 		this.position.sets[3].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_pos_right_lbp = 50;
-		// this.memfunc_pos_right_lpp = 150;
-		// this.memfunc_pos_right_lc = 5;
-		// this.memfunc_pos_right_rpp = 250;
-		// this.memfunc_pos_right_rbp = 350;
-		// this.memfunc_pos_right_rc = 5;
-		// this.memfunc_pos_right_name = "Right";
 		
 		// Far Right Member Function
 		this.position.sets[4].memfunc = new Object();
@@ -162,15 +126,6 @@ function FuzzyController() {
 		this.position.sets[4].memfunc.rbp = 650;
 		this.position.sets[4].memfunc.rc = 5;
 		
-		// OLD CODE
-		// this.memfunc_pos_farright_lbp = 250;
-		// this.memfunc_pos_farright_lpp = 350;
-		// this.memfunc_pos_farright_lc = 5;
-		// this.memfunc_pos_farright_rpp = 650;
-		// this.memfunc_pos_farright_rbp = 650;
-		// this.memfunc_pos_farright_rc = 5;
-		// this.memfunc_pos_farright_name = "Far Right";
-		
 		// ********************************************************************
 		// Line Velocity Member Function 
 		// ********************************************************************
@@ -179,91 +134,46 @@ function FuzzyController() {
 		this.velocity.sets[0].memfunc = new Object();
 		this.velocity.sets[0].memfunc.lbp = -650;
 		this.velocity.sets[0].memfunc.lpp = -650;
-		this.velocity.sets[0].memfunc.lc = 5;
+		this.velocity.sets[0].memfunc.lc = 0;
 		this.velocity.sets[0].memfunc.rpp = -350;
 		this.velocity.sets[0].memfunc.rbp = -250;
-		this.velocity.sets[0].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_vel_farleft_lbp = -650;
-		// this.memfunc_vel_farleft_lpp = -650;
-		// this.memfunc_vel_farleft_lc = 5;
-		// this.memfunc_vel_farleft_rpp = -350;
-		// this.memfunc_vel_farleft_rbp = -250;
-		// this.memfunc_vel_farleft_rc = 5;
-		//memfunc_vel_farleft_name = "Far Left";
+		this.velocity.sets[0].memfunc.rc = 0;
 		
 		// Left Member Function
 		this.velocity.sets[1].memfunc = new Object();
 		this.velocity.sets[1].memfunc.lbp = -350;
 		this.velocity.sets[1].memfunc.lpp = -250;
-		this.velocity.sets[1].memfunc.lc = 5;
+		this.velocity.sets[1].memfunc.lc = 0;
 		this.velocity.sets[1].memfunc.rpp = -150;
 		this.velocity.sets[1].memfunc.rbp = -50;
-		this.velocity.sets[1].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_vel_left_lbp = -350;
-		// this.memfunc_vel_left_lpp = -250;
-		// this.memfunc_vel_left_lc = 5;
-		// this.memfunc_vel_left_rpp = -150;
-		// this.memfunc_vel_left_rbp = -50;
-		// this.memfunc_vel_left_rc = 5;
-		// this.memfunc_vel_left_name = "Left";
+		this.velocity.sets[1].memfunc.rc = 0;
 		
 		// Center Member Function
 		this.velocity.sets[2].memfunc = new Object();
 		this.velocity.sets[2].memfunc.lbp = -150;
 		this.velocity.sets[2].memfunc.lpp = -50;
-		this.velocity.sets[2].memfunc.lc = 5;
+		this.velocity.sets[2].memfunc.lc = 0;
 		this.velocity.sets[2].memfunc.rpp = 50;
 		this.velocity.sets[2].memfunc.rbp = 150;
-		this.velocity.sets[2].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_vel_center_lbp = -150;
-		// this.memfunc_vel_center_lpp = -50;
-		// this.memfunc_vel_center_lc = 5;
-		// this.memfunc_vel_center_rpp = 50;
-		// this.memfunc_vel_center_rbp = 150;
-		// this.memfunc_vel_center_rc = 5;
-		// this.memfunc_vel_center_name = "Center";
+		this.velocity.sets[2].memfunc.rc = 0;
 		
 		// Right Member Function
 		this.velocity.sets[3].memfunc = new Object();
 		this.velocity.sets[3].memfunc.lbp = 50;
 		this.velocity.sets[3].memfunc.lpp = 150;
-		this.velocity.sets[3].memfunc.lc = 5;
+		this.velocity.sets[3].memfunc.lc = 0;
 		this.velocity.sets[3].memfunc.rpp = 250;
 		this.velocity.sets[3].memfunc.rbp = 350;
-		this.velocity.sets[3].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_vel_right_lbp = 50;
-		// this.memfunc_vel_right_lpp = 150;
-		// this.memfunc_vel_right_lc = 5;
-		// this.memfunc_vel_right_rpp = 250;
-		// this.memfunc_vel_right_rbp = 350;
-		// this.memfunc_vel_right_rc = 5;
-		// this.memfunc_vel_right_name = "Right";
+		this.velocity.sets[3].memfunc.rc = 0;
 		
 		// Far Right Member Function
 		this.velocity.sets[4].memfunc = new Object();
 		this.velocity.sets[4].memfunc.lbp = 250;
 		this.velocity.sets[4].memfunc.lpp = 350;
-		this.velocity.sets[4].memfunc.lc = 5;
+		this.velocity.sets[4].memfunc.lc = 0;
 		this.velocity.sets[4].memfunc.rpp = 650;
 		this.velocity.sets[4].memfunc.rbp = 650;
-		this.velocity.sets[4].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_vel_farright_lbp = 250;
-		// this.memfunc_vel_farright_lpp = 350;
-		// this.memfunc_vel_farright_lc = 5;
-		// this.memfunc_vel_farright_rpp = 650;
-		// this.memfunc_vel_farright_rbp = 650;
-		// this.memfunc_vel_farright_rc = 5;
-		// this.memfunc_vel_farright_name = "Far Right";
+		this.velocity.sets[4].memfunc.rc = 0;
 		
 		// ********************************************************************
 		// Action Member Function 
@@ -273,163 +183,82 @@ function FuzzyController() {
 		this.action.sets[0].memfunc = new Object();
 		this.action.sets[0].memfunc.lbp = -650;
 		this.action.sets[0].memfunc.lpp = -650;
-		this.action.sets[0].memfunc.lc = 5;
+		this.action.sets[0].memfunc.lc = 0;
 		this.action.sets[0].memfunc.rpp = -550;
 		this.action.sets[0].memfunc.rbp = -450;
-		this.action.sets[0].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_extremeleft_lbp = -650;
-		// this.memfunc_act_extremeleft_lpp = -650;
-		// this.memfunc_act_extremeleft_lc = 5;
-		// this.memfunc_act_extremeleft_rpp = -550;
-		// this.memfunc_act_extremeleft_rbp = -450;
-		// this.memfunc_act_extremeleft_rc = 5;
-		// this.memfunc_act_extremeleft_name = "Extreme Left";
+		this.action.sets[0].memfunc.rc = 0;
 		
 		// Large Left Member Function
 		this.action.sets[1].memfunc = new Object();
 		this.action.sets[1].memfunc.lbp = -600;
 		this.action.sets[1].memfunc.lpp = -500;
-		this.action.sets[1].memfunc.lc = 5;
+		this.action.sets[1].memfunc.lc = 0;
 		this.action.sets[1].memfunc.rpp = -400;
 		this.action.sets[1].memfunc.rbp = -300;
-		this.action.sets[1].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_largeleft_lbp = -600;
-		// this.memfunc_act_largeleft_lpp = -500;
-		// this.memfunc_act_largeleft_lc = 5;
-		// this.memfunc_act_largeleft_rpp = -400;
-		// this.memfunc_act_largeleft_rbp = -300;
-		// this.memfunc_act_largeleft_rc = 5;
-		// this.memfunc_act_largeleft_name = "Large Left";
+		this.action.sets[1].memfunc.rc = 0;
 		
 		// Left Member Function
 		this.action.sets[2].memfunc = new Object();
 		this.action.sets[2].memfunc.lbp = -450;
 		this.action.sets[2].memfunc.lpp = -350;
-		this.action.sets[2].memfunc.lc = 5;
+		this.action.sets[2].memfunc.lc = 0;
 		this.action.sets[2].memfunc.rpp = -250;
 		this.action.sets[2].memfunc.rbp = -150;
-		this.action.sets[2].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_left_lbp = -450;
-		// this.memfunc_act_left_lpp = -350;
-		// this.memfunc_act_left_lc = 5;
-		// this.memfunc_act_left_rpp = -250;
-		// this.memfunc_act_left_rbp = -150;
-		// this.memfunc_act_left_rc = 5;
-		// this.memfunc_act_left_name = "Left";
+		this.action.sets[2].memfunc.rc = 0;
 		
 		// Slight Left Member Function
 		this.action.sets[3].memfunc = new Object();
 		this.action.sets[3].memfunc.lbp = -300;
 		this.action.sets[3].memfunc.lpp = -200;
-		this.action.sets[3].memfunc.lc = 5;
+		this.action.sets[3].memfunc.lc = 0;
 		this.action.sets[3].memfunc.rpp = -100;
 		this.action.sets[3].memfunc.rbp = 0;
-		this.action.sets[3].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_slightleft_lbp = -300;
-		// this.memfunc_act_slightleft_lpp = -200;
-		// this.memfunc_act_slightleft_lc = 5;
-		// this.memfunc_act_slightleft_rpp = -100;
-		// this.memfunc_act_slightleft_rbp = 0;
-		// this.memfunc_act_slightleft_rc = 5;
-		// this.memfunc_act_slightleft_name = "Slight Left";
+		this.action.sets[3].memfunc.rc = 0;
 		
 		// Center Member Function
 		this.action.sets[4].memfunc = new Object();
 		this.action.sets[4].memfunc.lbp = -150;
 		this.action.sets[4].memfunc.lpp = -50;
-		this.action.sets[4].memfunc.lc = 5;
+		this.action.sets[4].memfunc.lc = 0;
 		this.action.sets[4].memfunc.rpp = 50;
 		this.action.sets[4].memfunc.rbp = 150;
-		this.action.sets[4].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_center_lbp = -150;
-		// this.memfunc_act_center_lpp = -50;
-		// this.memfunc_act_center_lc = 5;
-		// this.memfunc_act_center_rpp = 50;
-		// this.memfunc_act_center_rbp = 150;
-		// this.memfunc_act_center_rc = 5;
-		// this.memfunc_act_center_name = "None";
+		this.action.sets[4].memfunc.rc = 0;
 		
 		// Slight Right Member Function
 		this.action.sets[5].memfunc = new Object();
 		this.action.sets[5].memfunc.lbp = 0;
 		this.action.sets[5].memfunc.lpp = 100;
-		this.action.sets[5].memfunc.lc = 5;
+		this.action.sets[5].memfunc.lc = 0;
 		this.action.sets[5].memfunc.rpp = 200;
 		this.action.sets[5].memfunc.rbp = 300;
-		this.action.sets[5].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_slightright_lbp = 0;
-		// this.memfunc_act_slightright_lpp = 100;
-		// this.memfunc_act_slightright_lc = 5;
-		// this.memfunc_act_slightright_rpp = 200;
-		// this.memfunc_act_slightright_rbp = 300;
-		// this.memfunc_act_slightright_rc = 5;
-		// this.memfunc_act_slightright_name = "Slight Right";
+		this.action.sets[5].memfunc.rc = 0;
 		
 		// Right Member Function
 		this.action.sets[6].memfunc = new Object();
 		this.action.sets[6].memfunc.lbp = 150;
 		this.action.sets[6].memfunc.lpp = 250;
-		this.action.sets[6].memfunc.lc = 5;
+		this.action.sets[6].memfunc.lc = 0;
 		this.action.sets[6].memfunc.rpp = 350;
 		this.action.sets[6].memfunc.rbp = 450;
-		this.action.sets[6].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_right_lbp = 150;
-		// this.memfunc_act_right_lpp = 250;
-		// this.memfunc_act_right_lc = 5;
-		// this.memfunc_act_right_rpp = 350;
-		// this.memfunc_act_right_rbp = 450;
-		// this.memfunc_act_right_rc = 5;
-		// this.memfunc_act_right_name = "Right";
+		this.action.sets[6].memfunc.rc = 0;
 		
 		// Large Right Member Function
 		this.action.sets[7].memfunc = new Object();
 		this.action.sets[7].memfunc.lbp = 300;
 		this.action.sets[7].memfunc.lpp = 400;
-		this.action.sets[7].memfunc.lc = 5;
+		this.action.sets[7].memfunc.lc = 0;
 		this.action.sets[7].memfunc.rpp = 500;
 		this.action.sets[7].memfunc.rbp = 600;
-		this.action.sets[7].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_largeright_lbp = 300;
-		// this.memfunc_act_largeright_lpp = 400;
-		// this.memfunc_act_largeright_lc = 5;
-		// this.memfunc_act_largeright_rpp = 500;
-		// this.memfunc_act_largeright_rbp = 600;
-		// this.memfunc_act_largeright_rc = 5;
-		// this.memfunc_act_largeright_name = "Large Right";
+		this.action.sets[7].memfunc.rc = 0;
 		
 		// Extreme Right Member Function
 		this.action.sets[8].memfunc = new Object();
 		this.action.sets[8].memfunc.lbp = 450;
 		this.action.sets[8].memfunc.lpp = 550;
-		this.action.sets[8].memfunc.lc = 5;
+		this.action.sets[8].memfunc.lc = 0;
 		this.action.sets[8].memfunc.rpp = 650;
 		this.action.sets[8].memfunc.rbp = 650;
-		this.action.sets[8].memfunc.rc = 5;
-		
-		// OLD CODE
-		// this.memfunc_act_extremeright_lbp = 450;
-		// this.memfunc_act_extremeright_lpp = 550;
-		// this.memfunc_act_extremeright_lc = 5;
-		// this.memfunc_act_extremeright_rpp = 650;
-		// this.memfunc_act_extremeright_rbp = 650;
-		// this.memfunc_act_extremeright_rc = 5;
-		// this.memfunc_act_extremeright_name = "Extreme Right";
+		this.action.sets[8].memfunc.rc = 0;
 		
 		// ********************************************************************
 		// Rules 
@@ -508,6 +337,8 @@ function FuzzyController() {
     // Output:      fuzzy_value - value of the fuzzy membership
     // ********************************************************************
 	this.fuzzify = function(variable, set, input) {
+		
+		console.log("Fuzzifying variable = "+variable+", set="+set+", input="+input);
 	
 		// Determine points
 		var lbp = this[variable].sets[set].memfunc.lbp,
@@ -520,18 +351,21 @@ function FuzzyController() {
 		var	rc = this[variable].sets[set].memfunc.rc,
 			rpc = rpp+(this[variable].sets[set].memfunc.rc/10)*(rbp-rpp),
 			rbc = rbp-(this[variable].sets[set].memfunc.rc/10)*(rbp-rpp);
+		var fuzzy_value=0;
+		console.log("   lbp = "+lbp+", lpp="+lpp+", lc="+lc+", lbc="+lbc+", lpc="+lpc);
+		console.log("   rbp = "+rbp+", rpp="+rpp+", rc="+rc+", rpc="+rpc+", rbc="+rbc);
 		
 		// Below lbp - non-member
 		if (input <= lbp) {
-			return 0;
+			fuzzy_value=0;
 		}
 		
 		// Between lbp and lpp - in the sloped region of the graph
-		if (input > lbp && input < lpp) {
+		else if (input > lbp && input < lpp) {
 			// If there's no curviness, just use a straight line (faster)
 			if (lc == 0) {
 				var slope = (1)/(lpp-lbp);
-				return (input-lbp)*slope;
+				fuzzy_value = (input-lbp)*slope;
 			}
 			else { // use bezier curve equation to determine membership
 				// Find cubic root t from x coordinates
@@ -543,21 +377,21 @@ function FuzzyController() {
 				
 				// Given t, calculate y
 				var y = Math.pow((1-t),3) + 3*Math.pow((1-t),2)*t;
-				return y;
+				fuzzy_value = 1-y; // Why??????!!!!!
 			}
 		}
 		
 		// Between lpp and rpp - total membership
-		if (input >= lpp && input <= rpp) {
-			return 1;
+		else if (input >= lpp && input <= rpp) {
+			fuzzy_value = 1;
 		}
 		
 		// Between rpp and rbp - in the sloped region of the graph
-		if (input > rpp && input < rbp) {
+		else if (input > rpp && input < rbp) {
 			// If there's no curviness, just use a straight line (faster)
 			if (rc == 0) {
 				var slope = (1)/(rbp-rpp);
-				return (input-rpp)*slope;
+				fuzzy_value = (input-rpp)*slope;
 			}
 			else { // use bezier curve equation to determine membership
 				// Find cubic root t from x coordinates
@@ -569,14 +403,18 @@ function FuzzyController() {
 				
 				// Given t, calculate y
 				var y = Math.pow((1-t),3) + 3*Math.pow((1-t),2)*t;
-				return y;
+				fuzzy_value = y;
 			}
 		}
 		
 		// Above rbp - non-member
-		if (input >= rbp) {
-			return 0;
+		else if (input >= rbp) {
+			fuzzy_value = 0;
 		}
+		
+		// Return result
+		console.log("   Fuzzification result: fuzzy_value = "+fuzzy_value);
+		return fuzzy_value;
 	}
 	
     // ********************************************************************
@@ -595,20 +433,50 @@ function FuzzyController() {
 		console.log("      Depressed cubic values p="+p+", q="+q+", Q="+Q+", R="+R);
 		
 		// Solve for w
-		var w3 = (R + Math.sqrt(Math.pow(Q,3) + Math.pow(R,2)));
+		var w3 = (R + Math.sqrt(Q*Q*Q + R*R));
 		var w = Math.pow(w3,(1/3)); 
 		console.log("      w3="+w3+", w="+w);
 		
 		var t = w - p/(3*w);
 		var x = t - b/(3*a);
 		console.log("      t="+t+", x="+x);
+		
+		// As one equation
+		// x = -b/(3*a)
+			// - (1 / (3*a) ) 
+					// * Math.pow((0.5)
+								// *(2*b*b*b 
+									// - 9*a*b*c
+									// + 27*a*a*d
+									// + Math.sqrt( Math.pow((2*b*b*b
+															// -9*a*b*c
+															// +27*a*a*d),2
+												// )
+												// -4*Math.pow((b*b-3*a*c),3)
+									// )
+								// ),1/3)
+			// - (1 / (3*a) ) 
+					// * Math.pow((0.5)
+								// *(2*b*b*b 
+									// - 9*a*b*c
+									// + 27*a*a*d
+									// - Math.sqrt( Math.pow((2*b*b*b
+															// -9*a*b*c
+															// +27*a*a*d),2
+												// )
+												// -4*Math.pow((b*b-3*a*c),3)
+									// )
+								// ),1/3)
+		// console.log("      x="+x);
+		
 		return x;
 	}
 	
     // ********************************************************************
     // Function:    defuzzify()
     // Purpose:     Given a set membership, determine the action to be
-	//					taken.
+	//					taken. NOTE: Currently ignores the curviness
+	//					of output functions.
     // Input:       variable - The variable being processed
 	//				set - the set to check for membership
 	//				input - fuzzy set membership
@@ -657,46 +525,54 @@ function FuzzyController() {
 			
 			// Left side
 			// If there's no curviness, just use a straight line (faster)
-			if (lc == 0) {
+			//if (lc == 0) {
 				var slope = (1)/(lpp-lbp);
 				lx = lbp + input/slope;
-			} else { // use bezier curve equation to determine x
+			// } else { // use bezier curve equation to determine x
 				// var a = 0+3*0-3*1+1,
 					// b = 3*0-6*0+3*1,
 					// c = -3*0+3*0,
 					// d = 0 - input;
-				var a = -4,
-					b = 3,
-					c = 0,
-					d = 0-input;
-				var t = this.solveCubic(a,b,c,d);
-				console.log("   Cubic root t = "+t);
+				// var a = -2,
+					// b = 3,
+					// c = 0,
+					// d = -input;
+				// var t = this.solveCubic(a,b,c,d);
 				
-				// Given t, calculate x
-				lx = Math.pow((1-t),3)*lbp + 3*Math.pow((1-t),2)*t*lbc 
-						+ (1-t)*t*t*lpc + t*t*t*lpp;
-			}
+				// console.log("   Cubic root t = "+t);
+				
+				// t = 0.5 * (
+					// Math.pow(2*Math.sqrt(input*input - input)-2*input+1,(1/3))
+					// + 1 / (Math.pow((2*Math.sqrt(input*input - input)-2*input+1),(1/3))) + 1
+					// )
+				// console.log("   Wolfram t = "+t);
+				
+				
+				//Given t, calculate x
+				// lx = Math.pow((1-t),3)*lbp + 3*Math.pow((1-t),2)*t*lbc 
+						// + (1-t)*t*t*lpc + t*t*t*lpp;
+			// }
 			
 			// Left side
 			// If there's no curviness, just use a straight line (faster)
-			if (rc == 0) {
+			// if (rc == 0) {
 				var slope = (1)/(rbp-rpp);
 				rx = rpp + input/slope;
-			} else { // use bezier curve equation to determine x
-				// var a = 1+3*1-3*0+0,
+			// } else { // use bezier curve equation to determine x
+				// var a = -1+3*1-3*0+0,
 					// b = 3*1-6*1+3*0,
 					// c = -3*1+3*1,
 					// d = 1 - input;
-				var a = 4,
-					b = -3,
-					c = 0,
-					d = 1 - input;
-				var t = this.solveCubic(a,b,c,d);
+				// var a = 2,
+					// b = -3,
+					// c = 0,
+					// d = 1 - input;
+				// var t = this.solveCubic(a,b,c,d);
 				
-				// Given t, calculate x
-				rx = Math.pow((1-t),3)*rpp + 3*Math.pow((1-t),2)*t*rpc 
-						+ (1-t)*t*t*rbc + t*t*t*rbp;
-			}
+				//Given t, calculate x
+				// rx = Math.pow((1-t),3)*rpp + 3*Math.pow((1-t),2)*t*rpc 
+						// + (1-t)*t*t*rbc + t*t*t*rbp;
+			// }
 			
 			// Calculate value and area
 			result.value = lx+(rx-lx)/2;
